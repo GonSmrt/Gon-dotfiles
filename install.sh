2234ff561b5a4ee0200f2d8cd68364937d2a0b08
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-06-28 18:44:54 -0300

    Actualizamos install.sh para que al instalar no haga backups constantemente

--- install.sh
+++ install.sh
@@ -127,6 +127,13 @@ esac
 print_section "Creando backups"
 
 backup_file() {
+
+    # Si ya es un symlink, no hacemos backup
+    if [ -L "$1" ]; then
+	echo "✅ Ya es un symlink: $1"
+	return
+    fi
+
     if [ -e "$1" ]; then
         echo "💾 Backup: $1"
         mv "$1" "$1.backup.$(date +%s)"
@@ -136,6 +143,7 @@ backup_file() {
 backup_file "$HOME/.zshrc"
 backup_file "$HOME/.p10k.zsh"
 backup_file "$HOME/.gitconfig"
+
 
 # ------------------------
 # HOME  DOTFILES
