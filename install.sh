7d20b2746fdbade5ab55697a9653267dbb7f76d1
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-10 15:58:54 -0300

    Modulamos deps

--- install.sh
+++ install.sh
@@ -36,54 +36,8 @@ repo_setup
 # DEPENDENCIAS
 # ------------------------
 
-print_section "Instalando dependencias"
-
-install_deps_ubuntu() {
-    echo "📦 Instalando dependencias (apt)..."
-
-    sudo apt update
-
-    sudo apt install -y \
-        zsh \
-        git \
-        fastfetch \
-        btop \
-        cava \
-        curl \
-        wget \
-        unzip \
-        fontconfig
-}
-
-install_deps_arch() {
-    echo "📦 Instalando dependencias (pacman)..."
-
-    sudo pacman -Syu --needed \
-        zsh \
-        git \
-        fastfetch \
-        btop \
-        cava \
-        curl \
-        wget \
-        unzip \
-        fontconfig
-}
-
-
-case "$OS" in
-    ubuntu|linuxmint|debian)
-        install_deps_ubuntu
-        ;;
- 
-    arch|cachyos)
-       install_deps_arch
-        ;;
-
-   *)
-       echo "⚠️ Sistema no soportado para instalación automática de deps"
-        ;;
-esac
+source scripts/deps.sh
+install_deps
 
 
 
