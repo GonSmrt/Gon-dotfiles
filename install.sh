5f1e96b0a25ed8434d95dd1fa83c04f296541211
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-10 16:25:26 -0300

    Modulamos fuentes

--- install.sh
+++ install.sh
@@ -78,19 +78,8 @@ install_wallpaper
 # Fuentes
 #-----------------------
 
-print_section "Fuentes"
-
-echo "🔤 Instalando fuentes..."
-
-mkdir -p "$HOME/.local/share/fonts"
-
-cp -r "$REPO_DIR/resources/fonts/." "$HOME/.local/share/fonts/"
-
-echo "🔤 Actualizando caché de fuentes..."
-
-fc-cache -f
-
-echo "✅ Caché de fuentes actualizada"
+source scripts/fonts.sh
+install_fonts
 
 #------------------------
 # TEMAS E ICONOS
