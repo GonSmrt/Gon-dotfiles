f443d6af938499a09a24309b03d7e22654f2f9f7
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-10 16:08:17 -0300

    Modulamos buckups

--- install.sh
+++ install.sh
@@ -50,26 +50,8 @@ install_shell
 # BACKUPS
 # ------------------------
 
-print_section "Creando backups"
-
-backup_file() {
-
-    # Si ya es un symlink, no hacemos backup
-    if [ -L "$1" ]; then
-	echo "✅ Ya es un symlink: $1"
-	return
-    fi
-
-    if [ -e "$1" ]; then
-        echo "💾 Backup: $1"
-        mv "$1" "$1.backup.$(date +%s)"
-    fi
-}
-
-backup_file "$HOME/.zshrc"
-backup_file "$HOME/.p10k.zsh"
-backup_file "$HOME/.gitconfig"
-
+source scripts/buckups.sh
+Create_backups
 
 # ------------------------
 # HOME  DOTFILES
