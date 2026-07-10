c22e3ef0a77e7834f46b45e74ffdb9827c99d97b
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-10 16:14:55 -0300

    Modulamos config dotfiles

--- install.sh
+++ install.sh
@@ -64,19 +64,8 @@ install_hdotfiles
 # CONFIG DOTFILES
 #-------------------------
 
-print_section "Configuraciones"
-
-echo "⚙️ Instalando Fastfetch..."
-echo "⚙️ Instalando Kitty..."
-
-# fastfetch (más robusto que glob)
-mkdir -p "$HOME/.config/fastfetch"
-cp -r "$REPO_DIR/config/fastfetch/." "$HOME/.config/fastfetch/"
-
-# kitty (terminal mamalóna)
-mkdir -p "$HOME/.config/kitty"
-cp -r "$REPO_DIR/config/kitty/." "$HOME/.config/kitty/"
-
+source scripts/configdotfiles.sh
+config_dotfiles
 
 #-------------------------
 # WALLPAPER
