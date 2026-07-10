eb502fc077325e732cc1b9aaee5ee0446e5a590b
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-10 15:56:20 -0300

    Modulamos deps

---
+++ scripts/deps.sh
@@ -0,0 +1,54 @@
+install_deps() {
+
+print_section "Instalando dependencias"
+
+install_deps_ubuntu() {
+    echo "📦 Instalando dependencias (apt)..."
+
+    sudo apt update
+
+    sudo apt install -y \
+        zsh \
+        git \
+        fastfetch \
+        btop \
+        cava \
+        curl \
+        wget \
+        unzip \
+        fontconfig
+}
+
+install_deps_arch() {
+    echo "📦 Instalando dependencias (pacman)..."
+
+    sudo pacman -Syu --needed \
+        zsh \
+        git \
+        fastfetch \
+        btop \
+        cava \
+        curl \
+        wget \
+        unzip \
+        fontconfig \
+	zsh-theme-powerlevel10k
+	
+}
+
+
+case "$OS" in
+    ubuntu|linuxmint|debian)
+        install_deps_ubuntu
+        ;;
+ 
+    arch|cachyos)
+       install_deps_arch
+        ;;
+
+   *)
+       echo "⚠️ Sistema no soportado para instalación automática de deps"
+        ;;
+esac
+
+}
