ce3cd0f8a2fae15ebaef0139d30e3e975a025a67
Gonzalo Pereira <pgonzalo2015@gmail.com>
2026-07-18 23:34:00 -0300

    Actualizamos configdotfiles.sh para que guarde nanorc

--- scripts/configdotfiles.sh
+++ scripts/configdotfiles.sh
@@ -13,4 +13,29 @@ cp -r "$REPO_DIR/config/fastfetch/." "$HOME/.config/fastfetch/"
 mkdir -p "$HOME/.config/kitty"
 cp -r "$REPO_DIR/config/kitty/." "$HOME/.config/kitty/"
 
+apply_nanorc() {
+
+	echo "📝 Configurando nano..."
+
+	if [ -f "$REPO_DIR/config/nano/nanorc" ]; then
+
+	if [ -f /etc/nanorc ]; then
+		sudo cp /etc/nanorc /etc/nanorc.backup
+		echo "📦 Backup de /etc/nanorc creado"
+else
+		echo "📦 Backup de /etc/nanorc ya existe"
+
+fi
+
+
+		sudo cp "$REPO_DIR/config/nano/nanorc" /etc/nanorc
+		echo "✅ Configuración de nano aplicada"
+	else
+		echo "❌ nanorc no encontrado en el repo"
+	fi
+
+}
+
+apply_nanorc
+
 }
